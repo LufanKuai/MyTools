bed35c9f9e47ae9d841a2dc0be8eef1d91a261ed
Feinno <lufkuai@sina.com>
2017-02-15 13:33:03 +0800

    V1.0.1

--- MyTools/ViewController.h
+++ MyTools/ViewController.h
@@ -9,7 +9,9 @@
 #import <Cocoa/Cocoa.h>
 
 @interface ViewController : NSViewController
-
+@property (weak) IBOutlet NSTextField *oldTextFeild;
+@property (unsafe_unretained) IBOutlet NSTextView *showTextView;
+- (IBAction)tapChange:(id)sender;
 
 @end
 
--- MyTools/ViewController.m
+++ MyTools/ViewController.m
@@ -21,5 +21,17 @@
 
     // Update the view, if already loaded.
 }
-
+- (NSString *)replaceUnicode:(NSString *)unicodeStr {
+    NSString *tempStr1 = [unicodeStr stringByReplacingOccurrencesOfString:@"\\u" withString:@"\\U"];
+    NSString *tempStr2 = [tempStr1 stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+    NSString *tempStr3 = [[@"\"" stringByAppendingString:tempStr2] stringByAppendingString:@"\""];
+    NSData *tempData = [tempStr3 dataUsingEncoding:NSUTF8StringEncoding];
+    NSString* returnStr = [NSPropertyListSerialization propertyListWithData:tempData options:NSPropertyListImmutable format:NULL error:NULL];
+    return [returnStr stringByReplacingOccurrencesOfString:@"\\r\\n" withString:@"\n"];
+}
+- (IBAction)tapChange:(id)sender {
+    if (self.oldTextFeild.stringValue.length >0) {
+        self.showTextView.string = [self replaceUnicode:self.oldTextFeild.stringValue];
+    }
+}
 @end
